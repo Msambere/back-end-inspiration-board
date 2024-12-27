fba48e832f5cfad5a172f442421cdea3d9e28a34
Salma <salmayousry5@gmail.com>
2024-12-27 02:18:26 -0800

    Board Route: get a board/ cards & Board Route: update a board

--- app/models/board.py
+++ app/models/board.py
@@ -4,10 +4,11 @@ from app import db
 class Board(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     title: Mapped[str] = mapped_column(nullable=False)
+    owner: Mapped[str] = mapped_column(nullable=False)
     cards = relationship('Card', back_populates='board', cascade='all, delete-orphan')
 
     def to_dict(self):
-        result = {"id": self.id, "title": self.title, "cards": self.cards}
+        result = {"id": self.id, "title": self.title, "cards": self.cards, "owner": self.owner}
         cards = []
         for card in self.cards:
             cards.append(card.to_dict())
@@ -21,5 +22,6 @@ class Board(db.Model):
             cards = board_data.get("cards")
         return cls(
             title=board_data.get("title"),
+            owner=board_data.get("owner"),
             cards=cards
         )
--- app/routes/board_route.py
+++ app/routes/board_route.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, make_response, abort, request
 
-from app import db
+from app import db, Card
 from app.models.board import Board
 from app.routes.routes_utilities import validate_model, create_model
 
@@ -29,7 +29,48 @@ def get_all_board():
     boards = db.session.scalars(query).all()
     return [board.to_dict() for board in boards]
 
+@board_bp.get("/<board_id>")
+def get_board(board_id):
+    validate_model(Board, board_id)
+    query = db.select(Board).where(Board.id == board_id)
+    board = db.session.scalar(query)
 
+    if not board:
+        response = {"message": f"{board_id} not found"}
+        abort(make_response(response, 404))
+
+    return board.to_dict()
+
+@board_bp.get("/<board_id>/cards")
+def get_specific_board_all_cards_for(board_id):
+    validate_model(Board, board_id)
+    query = db.select(Board).where(Board.id == board_id)
+    board = db.session.scalar(query)
+
+    if not board:
+        response = {"message": f"{board_id} not found"}
+        abort(make_response(response, 404))
+
+    return board.to_dict()["cards"]
+
+
+@board_bp.patch("/<board_id>")
+def update_board(board_id):
+    validate_model(Board, board_id)
+    query = db.select(Board).where(Board.id == board_id)
+    board = db.session.scalar(query)
+
+    if board is None:
+        abort(make_response({"message": f"Board {board_id} not found"}, 404))
+    request_body = request.get_json()
+    if "title" in request_body:
+        board.title = request_body["title"]
+    if "owner" in request_body:
+        board.owner = request_body["owner"]
+    db.session.commit()
+    return {
+        "board": board.to_dict()
+    }
 
 @board_bp.delete("/<board_id>")
 def delete_board(board_id):
@@ -46,4 +87,6 @@ def delete_board(board_id):
     return {
         "details": f'Task {board_id} "{board.title}" successfully deleted'
     }
+
+
 
